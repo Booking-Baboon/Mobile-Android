85ee05f5f1363c11671df5c8a88fb5e4d072c76a
markomitos <markomitosevic9@gmail.com>
2023-11-13 15:36:20 +0100

    splash screen

---
+++ app/src/main/java/com/example/bookingapptim4/SplashScreen.java
@@ -0,0 +1,66 @@
+package com.example.bookingapptim4;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        if (isConnectedToInternet()) {
+            int SPLASH_TIME_OUT = 5000;
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+
+                    Intent intent = new Intent(SplashScreen.this, LoginScreen.class);
+
+                    startActivity(intent);
+
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        } else {
+            showInternetDialog();
+        }
+
+
+    }
+    private boolean isConnectedToInternet() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
+
+    private void showInternetDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("No internet connection. Do you want to connect?")
+                .setCancelable(false)
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finishAffinity();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
