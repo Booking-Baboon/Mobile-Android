9ad3f4456f7863a79d84f7ae1586b8fe58a2e3a2
markomitos <markomitosevic9@gmail.com>
2023-11-14 12:48:50 +0100

    log in and register screens

---
+++ app/src/main/java/com/example/bookingapptim4/GuestMainScreen.java
@@ -0,0 +1,14 @@
+package com.example.bookingapptim4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuestMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_main_screen);
+    }
+}
--- app/src/main/java/com/example/bookingapptim4/LoginScreen.java
+++ app/src/main/java/com/example/bookingapptim4/LoginScreen.java
@@ -2,13 +2,33 @@ package com.example.bookingapptim4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreen.this, GuestMainScreen.class);
+
+                startActivity(intent);
+            }
+        });
+        Button registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreen.this, RegisterScreen.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
