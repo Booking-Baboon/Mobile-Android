98eea6168149be703fcbd4978c5981d500c0d250
maric300 <branko.maric300@gmail.com>
2023-11-15 17:22:18 +0100

    feature: added toolbar and navigation

--- app/src/main/java/com/example/bookingapptim4/UI/Elements/Activities/GuestMainScreen.java
+++ app/src/main/java/com/example/bookingapptim4/UI/Elements/Activities/GuestMainScreen.java
@@ -1,16 +1,64 @@
 package com.example.bookingapptim4.UI.Elements.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.bookingapptim4.R;
+import com.example.bookingapptim4.databinding.ActivityGuestMainScreenBinding;
 
 public class GuestMainScreen extends AppCompatActivity {
+
+    private ActivityGuestMainScreenBinding binding;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guest_main_screen);
+        binding = ActivityGuestMainScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            // postavlja prikazivanje "strelice prema nazad" (back arrow)
+            // kao indikatora navigacije na lijevoj strani Toolbar-a.
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            // postavlja ikonu koja se prikazuje umjesto strelice prema nazad.
+            // U ovom slučaju, postavljena je ikona hamburger iz drawable resursa (ic_hamburger).
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_hamburger);
+            //vo omogućuje da se klikom na gumb 'home' na Toolbar-u
+            // aktivira povratak na prethodni zaslon.
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 }
