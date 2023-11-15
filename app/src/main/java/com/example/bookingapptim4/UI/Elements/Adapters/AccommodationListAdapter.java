72d37e4ad378a27607c84d096b83c9b13618e4d3
Ilija Bešlin <44114597+GearFifth@users.noreply.github.com>
2023-11-15 23:03:23 +0100

    Created guest main screen fragment with a list of accommodations

---
+++ app/src/main/java/com/example/bookingapptim4/UI/Elements/Adapters/AccommodationListAdapter.java
@@ -0,0 +1,95 @@
+package com.example.bookingapptim4.UI.Elements.Adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.bookingapptim4.R;
+import com.example.bookingapptim4.UI.Elements.Models.Accommodation;
+
+import java.util.ArrayList;
+
+public class AccommodationListAdapter extends ArrayAdapter<Accommodation> {
+    private ArrayList<Accommodation> accommodations;
+    public AccommodationListAdapter(Context context, ArrayList<Accommodation> accommodations) {
+        super(context, R.layout.accommodation_card, accommodations);
+        this.accommodations = accommodations;
+    }
+
+    @Override
+    public int getCount() {
+        return accommodations.size();
+    }
+
+    /*
+     * Ova metoda vraca pojedinacan element na osnovu pozicije
+     * */
+    @Nullable
+    @Override
+    public Accommodation getItem(int position) {
+        return accommodations.get(position);
+    }
+
+    /*
+     * Ova metoda vraca jedinstveni identifikator, za adaptere koji prikazuju
+     * listu ili niz, pozicija je dovoljno dobra. Naravno mozemo iskoristiti i
+     * jedinstveni identifikator objekta, ako on postoji.
+     * */
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    /*
+     * Ova metoda popunjava pojedinacan element ListView-a podacima.
+     * Ako adapter cuva listu od n elemenata, adapter ce u petlji ici
+     * onoliko puta koliko getCount() vrati. Prilikom svake iteracije
+     * uzece java objekat sa odredjene poziciuje (model) koji cuva podatke,
+     * i layout koji treba da prikaze te podatke (view) npr R.layout.product_card.
+     * Kada adapter ima model i view, prosto ce uzeti podatke iz modela,
+     * popuniti view podacima i poslati listview da prikaze, i nastavice
+     * sledecu iteraciju.
+     * */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Accommodation accommodation = getItem(position);
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.accommodation_card,
+                    parent, false);
+        }
+        LinearLayout accommodationCard = convertView.findViewById(R.id.accommodation_card);
+        TextView accommodationName = convertView.findViewById(R.id.accommodation_title);
+        TextView accommodationDescription = convertView.findViewById(R.id.accommodation_description);
+        TextView accommodationLocation = convertView.findViewById(R.id.accommodation_location);
+        TextView accommodationTotalPrice = convertView.findViewById(R.id.accommodation_total_price);
+        TextView accommodationPricePerNight = convertView.findViewById(R.id.accommodation_price_per_night);
+        TextView accommodationRating = convertView.findViewById(R.id.accommodation_rating);
+
+
+        if(accommodation != null){
+            accommodationName.setText(accommodation.getName());
+            accommodationDescription.setText(accommodation.getDescription());
+            accommodationLocation.setText(accommodation.getLocation());
+            accommodationTotalPrice.setText(Integer.toString(accommodation.getPrice())  + "e");
+            accommodationPricePerNight.setText(Integer.toString(accommodation.getPrice()) + "e/night");
+            accommodationRating.setText("4.5");
+            accommodationCard.setOnClickListener(v -> {
+                // Handle click on the item at 'position'
+                Log.i("ShopApp", "Clicked: " + accommodation.getName());
+                Toast.makeText(getContext(), "Clicked: " + accommodation.getName()  , Toast.LENGTH_SHORT).show();
+            });
+        }
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/bookingapptim4/UI/Elements/Fragments/FragmentGuestMainScreen.java
@@ -0,0 +1,122 @@
+package com.example.bookingapptim4.UI.Elements.Fragments;
+
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.bookingapptim4.UI.Elements.Activities.GuestMainScreen;
+import com.example.bookingapptim4.R;
+import com.example.bookingapptim4.UI.Elements.Models.Accommodation;
+import com.example.bookingapptim4.UI.Elements.ViewModels.AccommodationViewModel;
+import com.example.bookingapptim4.databinding.FragmentGuestMainScreenBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentGuestMainScreen#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentGuestMainScreen extends Fragment {
+
+    public static ArrayList<Accommodation> accommodations = new ArrayList<Accommodation>();
+    private AccommodationViewModel accommodationViewModel;
+    private FragmentGuestMainScreenBinding binding;
+
+    public static FragmentGuestMainScreen newInstance() {
+        return new FragmentGuestMainScreen();
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        accommodationViewModel = new ViewModelProvider(this).get(AccommodationViewModel.class);
+
+        binding = FragmentGuestMainScreenBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        prepareAccommodationsList(accommodations);
+
+        SearchView searchView = binding.searchText;
+        accommodationViewModel.getText().observe(getViewLifecycleOwner(), searchView::setQueryHint);
+
+        Button btnFilters = binding.btnFilters;
+        btnFilters.setOnClickListener(v -> {
+//            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity(), 0);
+//            View dialogView = getLayoutInflater().inflate(R.layout.bottom_sheet_filter, null);
+//            bottomSheetDialog.setContentView(dialogView);
+//            bottomSheetDialog.show();
+        });
+
+        Spinner spinner = binding.btnSort;
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.sort_array));
+        // Specify the layout to use when the list of choices appears
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+                dialog.setMessage("Change the sort option?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.v("ShopApp", (String) parent.getItemAtPosition(position));
+                                ((TextView) parent.getChildAt(0)).setTextColor(Color.MAGENTA);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = dialog.create();
+                alert.show();
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        FragmentTransition.to(FragmentAccommodationList.newInstance(accommodations), getActivity(), false, R.id.scroll_accommodations_list);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void prepareAccommodationsList(ArrayList<Accommodation> accommodations){
+        accommodations.add(new Accommodation("Sweet home", "Feels like a home", "Switzerland", 100));
+        accommodations.add(new Accommodation("Sweet home", "Feels like a home", "Switzerland", 150));
+        accommodations.add(new Accommodation("Sweet home", "Feels like a home", "Switzerland",30));
+        accommodations.add(new Accommodation("Sweet home", "Feels like a home", "Switzerland", 80));
+    }
+}
