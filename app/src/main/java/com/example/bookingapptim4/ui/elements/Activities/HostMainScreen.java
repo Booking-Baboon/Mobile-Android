f4d6d7b87e72d71538df2ae09ce4cb04584b2d8e
maric300 <branko.maric300@gmail.com>
2023-12-21 22:04:22 +0100

    feat: added host-toolbar

--- app/src/main/java/com/example/bookingapptim4/ui/elements/Activities/HostMainScreen.java
+++ app/src/main/java/com/example/bookingapptim4/ui/elements/Activities/HostMainScreen.java
@@ -1,16 +1,67 @@
 package com.example.bookingapptim4.ui.elements.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.bookingapptim4.R;
+import com.example.bookingapptim4.databinding.ActivityHostMainScreenBinding;
+import com.example.bookingapptim4.domain.models.users.User;
 
 public class HostMainScreen extends AppCompatActivity {
+
+    private ActivityHostMainScreenBinding binding;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_host_main_screen);
+        //setContentView(R.layout.activity_host_main_screen);
+        binding = ActivityHostMainScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        User host = getIntent().getParcelableExtra("USER");
+        System.out.println( "Successfully logged in as:" +  host.getEmail());
+        toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            // postavlja prikazivanje "strelice prema nazad" (back arrow)
+            // kao indikatora navigacije na lijevoj strani Toolbar-a.
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            // postavlja ikonu koja se prikazuje umjesto strelice prema nazad.
+            // U ovom slučaju, postavljena je ikona hamburger iz drawable resursa (ic_hamburger).
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_hamburger);
+            //vo omogućuje da se klikom na gumb 'home' na Toolbar-u
+            // aktivira povratak na prethodni zaslon.
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.host_toolbar, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 }
